59ad526ce37ea5f8fcfc2e4202597387781b85f0
Parfen <nparfenov97@gmail.com>
2022-10-29 23:39:48 +0500

    Complete Task

--- Program.cs
+++ Program.cs
@@ -4,9 +4,34 @@ namespace Program
 {
     class Program
     {
+        public static bool IsNumber(string line)
+        {
+            foreach (var item in line)
+                if (!char.IsDigit(item))
+                    return false;
+
+            return true;
+        }
+
+        public static int Summa(string line)
+        {
+            int sum = 0;
+
+            foreach (char item in line)
+                sum += item - 48;
+
+            return sum;
+        }
+
         public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите число: ");
+            string line = Console.ReadLine();
+
+            if (IsNumber(line))
+                Console.WriteLine($"Сумма чисел: {Summa(line)}");
+            else
+                Console.WriteLine("Некоррктное число!");
         }
     }
 }
